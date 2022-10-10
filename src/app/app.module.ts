bf9eaafd3831757d26922de791b84ef1cb7229ec
Ansen123 <ansenfp@gmail.com>
2022-10-10 12:28:01 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TudulistComponent } from './tudulist/tudulist.component';
 
+const appRoutes:Routes=[
+  {
+    path:"",component:TudulistComponent
+  }
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +21,8 @@ import { TudulistComponent } from './tudulist/tudulist.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tudulist/tudulist.component.ts
+++ src/app/tudulist/tudulist.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TudulistComponent implements OnInit {
 
   constructor() { }
-  tuduData=[
+  tuduList=[
     {
       "userId": 1,
       "id": 1,
